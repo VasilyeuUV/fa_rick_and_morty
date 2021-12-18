b9a9c004b9e13929275d2bad61301402c3fab2ae
VasilyeuUV <vasilyeu.uv@mail.ru>
2021-12-18 13:45:33 +0300

    r&m: search realization

--- lib/ui/pages/search_page.dart
+++ lib/ui/pages/search_page.dart
@@ -35,24 +35,65 @@ class _SearchPageState extends State<SearchPage> {
     /// состояние отслеживаем через метод watch
     final state = context.watch<CharacterBloc>().state;
 
-    return state.when(
-        // - состояние процесса загрузки персонажа
-        loading: () => Center(
-              child: Row(
-                // -- размещаем строку посередине
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  // -- индикатор загрузки персонажа
-                  CircularProgressIndicator(strokeWidth: 2),
-                  // -- отступ вправо
-                  SizedBox(width: 10),
-                  Text('Loading...'),
-                ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        // - отступы для строки поиска
+        Padding(
+          padding: const EdgeInsets.all(5.0),
+          // - строка поиска
+          child: TextField(
+            // -- текст текста
+            style: const TextStyle(color: Colors.white),
+            // -- цвет курсора
+            cursorColor: Colors.white,
+            // -- параметры оформления
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: const Color.fromRGBO(86, 86, 86, 0.8),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide.none,
               ),
+              // -- отображение иконки
+              prefixIcon: const Icon(Icons.search, color: Colors.white),
+              // -- текст подсказки
+              hintText: 'Search Name',
+              // -- стиль подсказки
+              hintStyle: const TextStyle(color: Colors.white),
             ),
-        loaded: (characterLoaded) => Text('${characterLoaded.info}'),
 
-        // - состояние ошибки
-        error: () => const Text('Nothing found...'));
+            /// Cобытие изменения текста
+            /// value - значение в строке поиска
+            onChanged: (value) {
+              // -- при осуществлении поиска обращаемся к нашему событию
+              context
+                  .read<CharacterBloc>()
+                  .add(CharacterEvent.fetch(name: value, page: 1));
+            },
+          ),
+        ),
+        state.when(
+            // - состояние процесса загрузки персонажа
+            loading: () => Center(
+                  child: Row(
+                    // -- размещаем строку посередине
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      // -- индикатор загрузки персонажа
+                      CircularProgressIndicator(strokeWidth: 2),
+                      // -- отступ вправо
+                      SizedBox(width: 10),
+                      Text('Loading...'),
+                    ],
+                  ),
+                ),
+            loaded: (characterLoaded) => Text('${characterLoaded.info}'),
+
+            // - состояние ошибки
+            error: () => const Text('Nothing found...')),
+      ],
+    );
   }
 }
