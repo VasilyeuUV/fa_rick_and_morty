a14b9c62c9b35dd73a2e21ee1c857dbfaa10aee1
VasilyeuUV <vasilyeu.uv@mail.ru>
2021-12-18 13:20:19 +0300

    r&m: states handling

--- lib/ui/pages/search_page.dart
+++ lib/ui/pages/search_page.dart
@@ -1,6 +1,9 @@
 // Страница поиска
+// При запуске приложения здесь будем получать данные о персонажах
 
+import 'package:fa_rick_and_morty/data/bloc/character_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -10,8 +13,46 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+// При инициализации состояния
+// лучшее место для первичного запроса к API Rick and Morty
+  @override
+  void initState() {
+    context
+        .read<CharacterBloc>()
+        // обращаемся к нашему событию
+        .add(const CharacterEvent.fetch(name: '', page: 1));
+    super.initState();
+  }
+
+// В зависимости от состояний будем менять пользовательский интерфейс
+// для этого будем использовать паттерн Matching,
+// который реализован в пакете freezed
+
+  /// Обработка состояний
   @override
   Widget build(BuildContext context) {
-    return Container();
+    /// state будет получать состояния из нашего bloc
+    /// состояние отслеживаем через метод watch
+    final state = context.watch<CharacterBloc>().state;
+
+    return state.when(
+        // - состояние процесса загрузки персонажа
+        loading: () => Center(
+              child: Row(
+                // -- размещаем строку посередине
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  // -- индикатор загрузки персонажа
+                  CircularProgressIndicator(strokeWidth: 2),
+                  // -- отступ вправо
+                  SizedBox(width: 10),
+                  Text('Loading...'),
+                ],
+              ),
+            ),
+        loaded: (characterLoaded) => Text('${characterLoaded.info}'),
+
+        // - состояние ошибки
+        error: () => const Text('Nothing found...'));
   }
 }
